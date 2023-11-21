68577a45fb044552185290e17945f4bb1cd202c4
Benjamin <kiptoo925@gmail.com>
2023-11-21 17:18:35 +0300

    submit message to google sheets functionality added

--- scripts.js
+++ scripts.js
@@ -54,6 +54,23 @@ removeMenu.addEventListener('click', ()=>{
     closeMenu();
 })
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwc6wmOkVt18tt3XUq3GcO4-b-1tocY1Rnk-klRuci8ibr1YCMyeSSYmwWNHQYF9XZ2Aw/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
 // sideMenu.addEventListener('click', ()=>{
 
 // })
